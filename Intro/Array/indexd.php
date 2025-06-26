74ab5fd80f87cb516cfd404a0c08c2d0d14875b1
AlamgirRejvi <alamgirrejvi65@gmail.com>
2025-06-26 15:08:07 +0600

    Update indexd.php

--- Intro/Array/indexd.php
+++ Intro/Array/indexd.php
@@ -10,8 +10,8 @@
         <h3>1. Indexed Array (numerical keys)</h3>
         <p> <?php
                 $fruits = array("Apple", "Banana", "Mango");
-                echo $fruits[0]. "<br>";
-                var_dump($fruits) // Output: Apple
+                echo $fruits[0]. "<br>"; // Output: Apple 
+                var_dump($fruits) //  Output: array(3) { [0]=> string(5) "Apple" [1]=> string(6) "Banana" [2]=> string(5) "Mango" }
                 
             ?>
 
